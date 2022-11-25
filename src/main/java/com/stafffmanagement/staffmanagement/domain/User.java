aea6460d52ce02d8c8ee515c8e192a60897fc8ab
Sinebi Innazo <sinebi.innazo@gmail.com>
2022-11-25 21:12:13 +0100

    Added Boiler plate

---
+++ src/main/java/com/stafffmanagement/staffmanagement/domain/User.java
@@ -0,0 +1,35 @@
+package com.stafffmanagement.staffmanagement.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String userId;
+    private String firstName;
+    private String lastname;
+    private String username;
+    private String password;
+    private String email;
+    private String profileImage;
+    private Date lastLoginDate;
+    private Date lastLoginDateDisplay;
+    private Date joinDate;
+    private String[] roles;
+    private String[] authorities;
+    private boolean isActive;
+    private boolean isNotLocked;
+}
